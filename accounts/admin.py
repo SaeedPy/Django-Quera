fdcbae78795cf2a6ad7e776443cc49641a60b22c
saeed <codewithsaeed1992@gmail.com>
2021-03-04 18:04:50 +0330

    customaize admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
 
-# Register your models here.
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active']
+    list_display_links = ['username']
+    list_editable = ['is_staff', 'is_active']
+    list_filter = ['gender', 'is_staff', 'is_superuser', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'gender', 'age', 'description')}),
+        (_('Contact info'), {'fields': ['phone', 'address']}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
