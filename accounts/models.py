7fc8edb938f26704262a4a809151003280909f89
saeed <codewithsaeed1992@gmail.com>
2021-03-05 12:54:11 +0330

    delete unnecessary fields

--- accounts/models.py
+++ accounts/models.py
@@ -3,23 +3,17 @@ from django.db import models
 
 
 class User(AbstractUser):
-
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female')
-    )
-    
-    address = models.TextField(blank=True, null=True)
-    age = models.PositiveIntegerField(blank=True, null=True)
-    date_joined = models.DateTimeField()
-    description = models.TextField(blank=True, null=True)
-    email = models.EmailField(blank=True, null=True, verbose_name='email address')
-    first_name = models.CharField(max_length=25, blank=True, null=True)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=1, blank=True, null=True)
-    is_active = models.BooleanField(default=True, verbose_name='active')
-    is_staff = models.BooleanField(default=False, verbose_name='staff status')
-    is_superuser = models.BooleanField(default=False, verbose_name='superuser status')
-    last_login = models.DateTimeField(blank=True, null=True)
-    password = models.CharField(max_length=20)
-    phone = models.CharField(max_length=15, blank=True, null=True)
-    username = models.CharField(max_length=25, unique=True)
+        )
+
+    address = models.TextField(null=True, blank=True)
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True, verbose_name='email address')
+    first_name = models.CharField(max_length=150, null=True, blank=True)
+    gender = models.CharField(max_length=1, null=True, blank=True, choices=GENDER_CHOICES)
+    last_login = models.DateTimeField(null=True, blank=True)
+    last_name = models.CharField(max_length=150, null=True, blank=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
