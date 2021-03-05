0c8b50aab857514a5da9c3d65e9ee64a15defd54
saeed <codewithsaeed1992@gmail.com>
2021-03-05 16:01:14 +0330

    add custom manager

--- charities/models.py
+++ charities/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 
 from accounts.models import User
 
@@ -19,20 +20,28 @@ class Charity(models.Model):
     reg_number = models.CharField(max_length=10)
 
 
-class Task(models.Model):
+class TaskManager(models.Manager):
+    def related_tasks_to_charity(self, user):
+        return self.filter(charity__user=user).exclude(charity__user=None)
 
+    def related_tasks_to_benefactor(self, user):
+        return self.filter(assigned_benefactor__user=user).exclude(assigned_benefactor__user=None)
+
+    def all_related_tasks_to_user(self, user):
+        return self.filter(Q(state='P') | Q(charity__user=user) | Q(assigned_benefactor__user=user))
+
+
+class Task(models.Model):
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female')
     )
-
     STATE_CHOICES = (
         ('P', 'Pending'),
         ('W', 'Waitting'),
         ('A', 'Assigend'),
         ('D', 'Done')
     )
-
     assigned_benefactor = models.ForeignKey(Benefactor, null=True, on_delete=models.SET_NULL)
     charity = models.ForeignKey(Charity, on_delete=models.CASCADE)
     age_limit_from = models.IntegerField(blank=True, null=True)
@@ -43,3 +52,4 @@ class Task(models.Model):
     state = models.CharField(choices=STATE_CHOICES, max_length=1, default='P')
     title = models.CharField(max_length=100)
     
+    objects = TaskManager()
